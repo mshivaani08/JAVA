58ba9f445cc920057fb952b951afbbccad5444d8
mshivaani08 <sm9616@srmist.edu.in>
2026-02-27 18:46:15 +0530

    Add files via upload

---
+++ public class OOPSBannerAppUC6 {.java
@@ -0,0 +1,49 @@
+public class OOPSBannerAppUC6 {
+
+    public static void main(String[] args) {
+
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
+
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + p[i] + "  " + s[i] + "  " + s[i]);
+        }
+    }
+
+    static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    static String[] getP() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
+    }
+}
